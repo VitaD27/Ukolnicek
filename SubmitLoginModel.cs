bb0ce81427ccba91bb68672b6f97405437e50053
VitaD27 <46784089+VitaD27@users.noreply.github.com>
2020-04-26 09:52:17 +0200

    Delete SubmitLoginModel.cs

--- SubmitLoginModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Ukolnicek.Models
-{
-    public class SubmitLoginModel
-    {
-        public string Login { get; set; }
-        public string Password { get; set; }
-    }
-}
